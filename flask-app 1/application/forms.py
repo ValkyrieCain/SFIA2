732b26a3117cb368bdcd8b932f1667fcff9269c6
ValkyrieCain <valkyriecain@qa.com>
2020-03-02 14:44:25 +0000

    02 03 20

--- flask-app 1/application/forms.py
+++ flask-app 1/application/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField,StringField,SubmitField,PasswordField,BooleanField,FormField
+from wtforms import StringField,SubmitField#,IntegerField,PasswordField,BooleanField,FormField
 from wtforms.validators import DataRequired,Length,EqualTo,ValidationError
-from application.models import Users, Superheroes
+#from application.models import Users, Superheroes
 
 class Objectclass(FlaskForm):
 	objectclass=StringField(validators=[DataRequired(),Length(min=4,max=6)])
 	submit=SubmitField('Generate SCP')
+	def validate_class(self, objectclass):
+		if objectclass.objectclass.data.lower()!= "safe" and objectclass.objectclass.data.lower()!= "euclid" and
+		 objectclass.objectclass.data.lower()!= "keter" and objectclass.objectclass.data.lower()!= "test":
+			raise ValidationError('Object Class incorrectly inputted. Please try again.')
 
 class Hero(FlaskForm):
 	publisher=StringField(validators=[DataRequired(),Length(max=30)])
