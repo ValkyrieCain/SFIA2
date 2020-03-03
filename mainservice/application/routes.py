09b92034a6af1a38e1984aef99ddf38e04dea731
ValkyrieCain <valkyriecain@qa.com>
2020-03-03 15:48:58 +0000

    03 03 20

--- mainservice/application/routes.py
+++ mainservice/application/routes.py
@@ -8,21 +8,21 @@ from requests import *
 @app.route('/home')
 def home():
 	objectclass=Objectclass()
-	oc=""
+	oc="x"
 	no=get('http://sfia2_flask2_1:5000/no')
 	site=get('http://sfia2_flask2_1:5000/site')
 	container=get('http://sfia2_flask2_1:5000/container')
 	locker=get('http://sfia2_flask2_1:5000/locker')
 
 	if objectclass.validate_on_submit():
-		if objectclass.objectclass.data.lower()=="safe":
-			oc="Safe"
-		if objectclass.objectclass.data.lower()=="euclid":
-			oc="Euclid"
-		if objectclass.objectclass.data.lower()=="keter":
-			oc="Keter"
-		if objectclass.objectclass.data.lower()=="test":
-			oc="Thaumiel"
+		#if objectclass.objectclass.data.lower()=="safe":
+		#	oc="Safe"
+		#if objectclass.objectclass.data.lower()=="euclid":
+		#	oc="Euclid"
+		#if objectclass.objectclass.data.lower()=="keter":
+		#	oc="Keter"
+		#if objectclass.objectclass.data.lower()=="test":
+		#	oc="Thaumiel"
 		return render_template("scp.html",oc=oc,no=no,site=site,container=container,locker=locker)
 
 	return render_template("home.html", no=no, objectclass=objectclass)
