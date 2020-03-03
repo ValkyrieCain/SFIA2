66aa850ca22f78004f57a8e6d8dab142e84b3c3a
ValkyrieCain <valkyriecain@qa.com>
2020-03-03 09:41:03 +0000

    03 03 20

---
+++ keter.py
@@ -0,0 +1,16 @@
+from flask import render_template, redirect, url_for, request, flash
+from application import app
+from random import *
+import time
+@app.route('/no')
+def no():
+	return randint(1,10000)
+@app.route('/site')
+def site():
+	return randint(1,200)
+@app.route('/container')
+def container():
+	return randint(1,20)
+@app.route('/locker')
+def locker():
+	return randint(1,200)
---
+++ main service/application/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template, redirect, url_for, request, flash
+#from flask_login import login_user, current_user, logout_user, login_required
+from application import app, db
+#from application.models import Superheroes, Users, Powers
+from application.forms import Objectclass#, Hero, Search, Register, Login, Delete, Alterego, Alteregocreate, Dontdelete, Multidelete
+import time
+@app.route('/')
+@app.route('/home')
+def home():
+	if objectclass.validate_on_submit():
+		return redirect(url_for('scp'))
+	return render_template("home.html")
+@app.route('/scp', methods=['GET','POST'])
+def scp():
+	return render_template("scp.html", objectclass=objectclass)
+
+#<form method='POST' action=''>
+#  {{alterego.hidden_tag()}}
+#  {{alterego.submit}}<br>
+#</form>
