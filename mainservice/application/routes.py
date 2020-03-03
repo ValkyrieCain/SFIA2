8dcb9573feedf4d7f6eac17765972e729daf52f4
ValkyrieCain <valkyriecain@qa.com>
2020-03-03 16:26:58 +0000

    03 03 20

--- mainservice/application/routes.py
+++ mainservice/application/routes.py
@@ -15,17 +15,17 @@ def home():
 	locker=get('http://sfia2_flask2_1:5000/locker')
 
 	if objectclass.validate_on_submit():
-		#if objectclass.objectclass.data.lower()=="safe":
-		#	oc="Safe"
-		#if objectclass.objectclass.data.lower()=="euclid":
-		#	oc="Euclid"
-		#if objectclass.objectclass.data.lower()=="keter":
-		#	oc="Keter"
-		#if objectclass.objectclass.data.lower()=="test":
-		#	oc="Thaumiel"
+		if objectclass.objectclass.data.lower()=="safe":
+			oc="Safe"
+		if objectclass.objectclass.data.lower()=="euclid":
+			oc="Euclid"
+		if objectclass.objectclass.data.lower()=="keter":
+			oc="Keter"
+		if objectclass.objectclass.data.lower()=="test":
+			oc="Thaumiel"
 		return render_template("scp.html",oc=oc,no=no,site=site,container=container,locker=locker)
 
-	return render_template("home.html", no=no, objectclass=objectclass)
+	return render_template("home.html", objectclass=objectclass)
 
 #<form method='POST' action=''>
 #  {{alterego.hidden_tag()}}
