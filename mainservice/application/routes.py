59fab9fdee2e35671c872454092b58aa61ee09f4
ValkyrieCain <valkyriecain@qa.com>
2020-03-03 14:28:27 +0000

    03 03 20

--- mainservice/application/routes.py
+++ mainservice/application/routes.py
@@ -7,11 +7,11 @@ from requests import *
 @app.route('/')
 @app.route('/home')
 def home():
-	response = get('http://sfia2_flask2_1:5000/no')
+	#response = get('http://sfia2_flask2_1:5000/no')
 	print(response.text)
 	#if objectclass.validate_on_submit():
 		#return redirect(url_for('scp'))
-	return render_template("xx.html", response=response)
+	return render_template("xx.html")#, response=response)
 #@app.route('/scp', methods=['GET','POST'])
 #def scp():
 #	return render_template("scp.html", objectclass=objectclass)
