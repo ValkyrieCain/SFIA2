c8b5cdc84812f7ee5b2784560aea2aac9535686f
ValkyrieCain <valkyriecain@qa.com>
2020-03-05 09:24:08 +0000

    05 03 20

--- thaumiel/application/routes.py
+++ thaumiel/application/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request, flash
 from application import app
 from random import *
 @app.route('/thcontain')
-def thcontain():
+def thcontain(data):
 	if data=="safe":
 		return str(randint(1,200))
 	elif data=="euclid":
