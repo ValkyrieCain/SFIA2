46e1c75aafe3ee8ee93bfebd14d771bcfc5b161b
ValkyrieCain <valkyriecain@qa.com>
2020-03-05 13:31:35 +0000

    05 03 20

--- thaumiel/application/routes.py
+++ thaumiel/application/routes.py
@@ -1,13 +1,13 @@
-from flask import render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, request, flash, Response
 from application import app
 from random import *
-@app.route('/thcontain')
-def thcontain(data):
-	if data=="safe":
+@app.route('/thcontain', methods=['POST'])
+def thcontain():
+	if request.data.decode("utf-8")=="safe":
 		return str(randint(1,100))
-	elif data=="euclid":
+	elif request.data.decode("utf-8")=="euclid":
 		return str(randint(1,20))
-	elif data=="keter":
+	elif request.data.decode("utf-8")=="keter":
 		return str(randint(1,2))
 	else:
 		return "0"
