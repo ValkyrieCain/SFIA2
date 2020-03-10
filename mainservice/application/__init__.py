a2347f1086e4ba6a18e931ab611f7d549bf09cb5
ValkyrieCain <valkyriecain@qa.com>
2020-03-10 14:39:45 +0000

    10 03 20

--- mainservice/application/__init__.py
+++ mainservice/application/__init__.py
@@ -4,4 +4,5 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
+db = SQLAlchemy(app)
 from application import routes
