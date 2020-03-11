0bec32e1448a1c0727846829e7bc1ce34a1ca036
ValkyrieCain <valkyriecain@qa.com>
2020-03-11 09:23:44 +0000

    10 03 20

--- mainservice/application/routes.py
+++ mainservice/application/routes.py
@@ -7,13 +7,13 @@ import time
 from requests import *
 @app.route('/scp', methods=['GET','POST'])
 def scp():
-	maxid=Scp.query.filter(func.max(Scp.id)).first()
+	maxid=Scp.query.filter(max(Scp.id)).first()
 	no=Scp.query.filter(Scp.id==maxid.id).first()
 	scp="SCP-"+str(no.number)
 	return scp
 @app.route('/site', methods=['GET','POST'])
 def site():
-	maxid=Scp.query.filter(func.max(Scp.id)).first()
+	maxid=Scp.query.filter(max(Scp.id)).first()
 	siteget=Scp.query.filter(Scp.id==maxid.id).first()
 	site="Site "+str(siteget.site)
 	return site
