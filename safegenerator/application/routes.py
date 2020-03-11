a213c44fba2a2559c959e430e531fd0c2d14e86c
ValkyrieCain <valkyriecain@qa.com>
2020-03-11 11:25:11 +0000

    10 03 20

--- safegenerator/application/routes.py
+++ safegenerator/application/routes.py
@@ -21,12 +21,12 @@ def scategory():
 	return choice[0]
 @app.route('/sanomaly')
 def sanomaly():
-	no=get('http://sfia2_number_1:5000/no')
-	scp="SCP-"+str(no.text)
-	siteget=get('http://sfia2_number_1:5000/site')
+	no=get('http://sfia2_main_1:5000/scp')
+	scp=no.text
+	siteget=get('http://sfia2_main_1:5000/site')
 	site=siteget.text
 	anomaly=["causing those who look at it to percieve it as a magnifying glass.","██████████ ███████ ████████ ████",
 	"heating the air in a one meter radius around it to exactly twenty five degrees celcius", "creating a subspace bubble that absorbs all light",
-	"causing seven Foundation terminals at Site x to display an image of x at random intervals", "e", "f", "g"]
+	"causes seven Foundation terminals at "+site+" to display an image of "+scp+" at random intervals", "e", "f", "g"]
 	choice=sample(anomaly,1)
 	return choice[0]
